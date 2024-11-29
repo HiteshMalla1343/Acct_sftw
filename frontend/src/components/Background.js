3c338044396c67edbcfafce8e4cf7030d19c6cc4
Hitesh Malla <hiteshmalla@Hiteshs-MacBook-Air-7.local>
2024-11-29 17:46:27 +0530

    Used components

---
+++ frontend/src/components/Background.js
@@ -0,0 +1,13 @@
+// src/Background.js
+import React from 'react';
+import '../css/Background.css';
+
+function Background() {
+  return (
+    <div className="background-container">
+      {/* <h1>Welcome to My App</h1> */}
+    </div>
+  );
+}
+
+export default Background;
